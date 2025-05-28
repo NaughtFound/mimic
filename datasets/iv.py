efd5b1370a3bd6eb56382ae6a3a10a552c2d979f
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-05-28 10:25:10 +0330

    duckdb added to MIMIC_IV_Sheet

--- datasets/iv.py
+++ datasets/iv.py
@@ -1,18 +1,100 @@
 import os
 from pathlib import Path
-from typing import Union
+from typing import Any, Union
 from torch.utils.data import Dataset
 from torchvision.datasets.utils import check_integrity
+import duckdb
+import pandas as pd
 from utils import download_and_extract_archive
 from utils.env import Env
+
+
+class MIMIC_IV_Sheet:
+    def __init__(
+        self,
+        root: str,
+        db_path: str,
+        table_name: str,
+        columns: dict[str, str],
+        id_column: str,
+        transform_columns: list[str] = None,
+        scaler: Any = None,
+        train: bool = True,
+    ):
+        self.root = root
+        self.table_name = table_name
+        self.columns = columns
+        self.id_column = id_column
+        self.transform_columns = transform_columns
+        self.scaler = scaler
+        self.train = train
+
+        self.source_csv_path = os.path.join(root, f"{table_name}.csv")
+        self.db_path = db_path
+
+        self.connection = duckdb.connect(database=self.db_path, read_only=False)
+
+        self._create_table()
+
+    def _create_table(self):
+        columns_str = ", ".join(f"{col} {dtype}" for col, dtype in self.columns.items())
+        create_query = f"CREATE TABLE IF NOT EXISTS {self.table_name} ({columns_str});"
+        self.connection.execute(create_query)
+
+    def _transform_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        if not self.transform_columns:
+            return df
+
+        if not self.scaler:
+            return df
+
+        missing_columns = [
+            col for col in self.transform_columns if col not in df.columns
+        ]
+        if missing_columns:
+            raise ValueError(f"Columns {missing_columns} not found in CSV.")
+
+        if self.train:
+            self.scaler.fit(df[self.transform_columns])
+
+        df[self.transform_columns] = self.scaler.transform(df[self.transform_columns])
+
+        return df
+
+    def _insert_data(self, csv_path: str):
+        insert_query = (
+            f"COPY {self.table_name} FROM '{csv_path}' (FORMAT CSV, HEADER TRUE);"
+        )
+        self.connection.execute(insert_query)
+
+    def load_csv(self, csv_path: str):
+        df = pd.read_csv(csv_path, usecols=self.columns.keys(), dtype=self.columns)
+
+        if self.id_column not in df.columns:
+            raise ValueError(f"CSV file must contain an '{self.id_column}' column.")
+
+        df = self._transform_data(df)
+
+        csv_path = self.source_csv_path
+
+        if self.scaler is not None:
+            os.makedirs(os.path.join(self.root, "transformed"), exist_ok=True)
+            csv_path = os.path.join(self.root, "transformed", f"{self.table_name}.csv")
+
+        df.to_csv(csv_path, index=False)
+
+        self._insert_data(csv_path)
+
+    def close(self):
+        self.connection.close()
 
 
 class MIMIC_IV(Dataset):
     def __init__(
         self,
         root: Union[str, Path],
+        sheets: dict[str, MIMIC_IV_Sheet],
         download: bool = False,
-        download_whitelist: list[str] = None,
     ):
         super().__init__()
 
@@ -20,14 +102,12 @@ class MIMIC_IV(Dataset):
 
         self.root = root
         self.credentials = env.credentials
+        self.sheets = sheets
 
-        if download_whitelist is not None:
-            self.resources = []
-            for f in env.iv_files:
-                if f["name"] in download_whitelist:
-                    self.resources.append(f)
-        else:
-            self.resources = env.iv_files
+        self.resources = []
+        for f in env.iv_files:
+            if f["name"] in list(sheets.keys()):
+                self.resources.append(f)
 
         if download:
             self.download()
@@ -37,11 +117,14 @@ class MIMIC_IV(Dataset):
                 "Dataset not found. You can use download=True to download it"
             )
 
+        self._load_data()
+
     def _check_exists(self) -> bool:
         return all(
             check_integrity(
                 os.path.join(
-                    self.raw_folder, os.path.splitext(os.path.basename(f["url"]))[0]
+                    self.raw_folder,
+                    os.path.splitext(os.path.basename(f["url"]))[0],
                 )
             )
             for f in self.resources
@@ -64,3 +147,9 @@ class MIMIC_IV(Dataset):
                 credentials=self.credentials,
                 md5=f["md5"],
             )
+
+    def _load_data(self):
+        self.data = {}
+
+        for sheet in self.sheets:
+            pass
